c28a6084df6a486ca93673e6deb9713c3a209744
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2023-08-31 13:13:40 +0000

    Deploying to gh-pages from @ starodubbb/command-project-js@0f0b13a85cf03977786bdea21d087f3bacbd1fe8 🚀

---
+++ index.142170c9.js
@@ -0,0 +1,2 @@
+!function(){const o=document.getElementById("scrollBtn");window.onscroll=function(){document.body.scrollTop>200||document.documentElement.scrollTop>200?o.style.display="flex":o.style.display="none"},o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.142170c9.js.map
