aa3a56a9d3a81a9c2a700f4cad820f90d1fd9e6e
starodubbb <90095788+starodubbb@users.noreply.github.com>
2023-09-02 10:43:44 +0000

    Deploying to gh-pages from @ starodubbb/command-project-js@a969c3538695b49dba94ebb869e8781ebc2ba1a5 🚀

---
+++ shopping-list-page.598c90da.js
@@ -0,0 +1,2 @@
+!async function(){const o=document.getElementById("scrollBtn");window.onscroll=function(){document.body.scrollTop>200||document.documentElement.scrollTop>200?o.style.display="flex":o.style.display="none"},o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=shopping-list-page.598c90da.js.map
