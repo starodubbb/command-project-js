7f7298512b20bb9d31958e237aed2cd5f2b5c23d
starodubbb <e.molodid2011@gmail.com>
2023-09-02 13:37:48 +0300

    add required files

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
+import './header.js';
 import './pop-up.js';
 import './scroll-up.js';
+import './sidebar.js';
+import './support.js';
+import './best-sellers.js';
+import './mobile-menu.js';
+import './service-api.js';
