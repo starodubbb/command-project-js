ee48bdbd45c3cfbc235ebfb8ff1b414f791ebfa6
starodubbb <e.molodid2011@gmail.com>
2023-09-03 20:59:51 +0300

    fixes header and mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,12 +1,19 @@
 const btnMenu = document.querySelector('[data-menu-button]');
 const headerMobile = document.querySelector('[data-menu]');
+const bodyRef = document.querySelector('body');
+const anchorRef = document.querySelectorAll('.mobile-menu-nav-li');
 
-btnMenu.onclick = function () {
+btnMenu.addEventListener('click', doit);
+for (let i = 0; i < anchorRef.length; i++) {
+  anchorRef[i].addEventListener('click', doit);
+}
+
+function doit() {
+  const expanded = btnMenu.getAttribute('aria-expanded') === 'true' || false;
+  btnMenu.setAttribute('aria-expanded', !expanded);
+
+  btnMenu.classList.toggle('is-open');
+
   headerMobile.classList.toggle('show');
-
-  if (headerMobile.classList.contains('show')) {
-    headerMobile.setAttribute('aria-expanded', 'true');
-  } else {
-    headerMobile.setAttribute('aria-expanded', 'false');
-  }
-};
+  bodyRef.classList.toggle('overflow-hidden');
+}
