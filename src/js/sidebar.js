ae0c17a01d328c23dc4ad0f71c0ecebba8430762
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-09-03 15:55:59 +0300

    change side-bar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,41 +1,45 @@
-import { fetchCategoryList, fetchParticularCategory} from './service-api';
+import { fetchCategoryList, fetchParticularCategory } from './service-api';
 
 const sideBarEl = document.querySelector('.side-bar');
 const sideBarListEl = document.querySelector('.side-bar-list');
 const containerEl = document.querySelector('.container');
-
 
 startRender();
 
 async function startRender() {
   const data = await fetchCategoryList();
 
-  const markupTitle = `<h2 class="side-bar-title current-category">All categories</h2>`;
-sideBarEl.insertAdjacentHTML('afterbegin', markupTitle)
+  const markupTitle = `<li><button class="side-bar-btn current-category" type="button">All categories</button></li>`;
+  sideBarListEl.insertAdjacentHTML('afterbegin', markupTitle);
 
   renderMarkupList(data);
 }
 
 function renderMarkupList(arrays) {
-  const markup = arrays.map(({ list_name }) => {
-    return `<li><button type="button" class="side-bar-btn">${list_name}</button></li>`;
-  }).join("");
+  const markup = arrays
+    .map(({ list_name }) => {
+      return `<li><button type="button" class="side-bar-btn">${list_name}</button></li>`;
+    })
+    .join('');
   return sideBarListEl.insertAdjacentHTML('beforeend', markup);
 }
 
-
 sideBarListEl.addEventListener('click', onRenderMarkup);
 
-function onRenderMarkup (e) {
-	const currentCategory = e.target.textContent;
-	
+function onRenderMarkup(e) {
+  const currentCategory = e.target.textContent;
 
-	renderMarkupBook(currentCategory);
+  renderMarkupBook(currentCategory);
 
+  e.target.classList.add('current-category');
 }
 
-async function renderMarkupBook (category) {
-	const data = await fetchParticularCategory(category);
-	console.log(data)
-}
+async function renderMarkupBook(category) {
+  const data = await fetchParticularCategory(category);
+  console.log(data);
 
+  const markupBook = data.map(book => {
+	return `<li class=card-set-item>
+	</li`
+  })
+}
